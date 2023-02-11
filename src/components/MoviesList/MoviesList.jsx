0e3ab7b4e8b9042578856ae50686454b46c7f104
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-02-11 18:09:21 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,17 +2,23 @@ import { Link } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
 
-export const MoviesList = ({movies}) => (
-    <>
-        <ul>
-            {movies.map(({id, title, name, original_title, original_name}) =>
-                <li key={id}>
-                    <Link>{title ?? name ?? original_title ?? original_name}</Link>
-                </li>
-            )}
-        </ul>
-    </>
-);
+export const MoviesList = ({movies}) => {
+
+
+    return (
+        <>
+            <ul>
+                {movies.map(({id, title, name, original_title, original_name}) =>
+                    <li key={id}>
+                        <Link to={`/movies/${id}`}>
+                            {title ?? name ?? original_title ?? original_name}
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </>
+    );
+};
 
 MoviesList.propTypes = {
     movies: PropTypes.array.isRequired,
