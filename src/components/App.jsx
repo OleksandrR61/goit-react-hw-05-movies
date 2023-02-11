4b933bb26fdba3b23de93f908c34f833995587d2
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-02-11 11:21:06 +0200

    23/02/11/11/20

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { Layout } from 'components/Layout/Layout';
 import Home from 'pages/Home/Home';
 import Movies from 'pages/Movies/Movies';
 import MovieDetails from 'pages/MovieDetails/MovieDetails';
@@ -9,13 +10,15 @@ import NotFound from 'pages/NotFound/NotFound';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/movies' element={<Movies />} />
-      <Route path='/movies/:movieId' element={<MovieDetails />} />
-      <Route path='/movies/:movieId/cast' element={<Cast />}/>
-      <Route path='/movies/:movieId/reviews' element={<Reviews />}/>
-      <Route path='*' element={<NotFound />}/>
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:movieId' element={<MovieDetails />} />
+        <Route path='/movies/:movieId/cast' element={<Cast />}/>
+        <Route path='/movies/:movieId/reviews' element={<Reviews />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </Layout>
   );
 };
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom';
+
+import styles from './Layout.module.css';
+import styled from 'styled-components';
+
+const StyledNavLink = styled(NavLink)`
+    margin: 0 8px;
+    padding: 4px;
+
+    &.active {
+        color: red;
+    }
+`;
+
+export const Layout = ({children}) => (
+    <>
+        <header className={styles.header}>
+            <h1>Movies</h1>
+            <nav>
+                <StyledNavLink to='/'>Home</StyledNavLink>
+                <StyledNavLink to='/movies'>Movies</StyledNavLink>
+            </nav>
+        </header>
+        {children}
+    </>
+);
